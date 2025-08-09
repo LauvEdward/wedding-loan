2019465f6c9a80036e71951cb2d7afac6a0f111c
Lê Đình Phục <Edwardlauv@gmail.com>
2025-08-09 22:23:03 +0700

    build code

--- src/Countdown.tsx
+++ src/Countdown.tsx
@@ -1,23 +1,6 @@
-import { useEffect, useState } from 'react'
 import './Countdown.css'
 
-const targetDate = new Date('2025-07-18T00:00:00')
-
 const Countdown = () => {
-  const [daysLeft, setDaysLeft] = useState(0)
-
-  useEffect(() => {
-    const updateCountdown = () => {
-      const now = new Date()
-      const diff = targetDate.getTime() - now.getTime()
-      const days = Math.ceil(diff / (1000 * 60 * 60 * 24))
-      setDaysLeft(days)
-    }
-
-    updateCountdown()
-    const interval = setInterval(updateCountdown, 1000 * 60) // cập nhật mỗi phút
-    return () => clearInterval(interval)
-  }, [])
 
   return (
     <div className="countdown">
